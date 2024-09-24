0dcb990773f91e9a80db0c0cac2cb1da1d333f12
Karinka_ter <tigrantervartanyan@gmail.com>
2024-09-24 13:26:28 +0500

    lesson-5

--- lesson-05/02.js
+++ lesson-05/02.js
@@ -35,4 +35,17 @@ const gallery = {
   'The Scream': 'Edvard Munch',
 }
 
-function updateGallery() {}
+function updateGallery(obj,key1,newMeaning) {
+  for (let key in obj){
+    if(key === key1){
+      obj[key] = newMeaning
+    } else {
+      obj[key1] = newMeaning
+    }
+  }
+}
+
+updateGallery(gallery, 'Mona Lisa', 'Leonardo da Vinci, 1503-1506')
+updateGallery(gallery, 'The Persistence of Memory', 'Salvador Dali')
+
+console.log(gallery)
